9905511555d440accc27d19344171f879ec46c4c
Sebastian Bobadilla Suarez <sebastian.suarez.12@ucl.ac.uk>
2018-02-22 13:57:16 +0000

    trying to get the variation of information right

--- distance_measures.py
+++ distance_measures.py
@@ -12,6 +12,9 @@ import numpy as np
 from sklearn.covariance import ledoit_wolf
 
 def var_info(mat1, mat2,reg=0):
+    #Variation of Information assuming normal distros
+    #expects rows to be observations and columns features    
+    #needs checking....
     n = mat1.shape[1]
     mat0 = np.hstack([mat1,mat2])
     if(reg==1):
@@ -22,16 +25,11 @@ def var_info(mat1, mat2,reg=0):
         cov_mat0 = np.cov(mat0)
         cov_mat1 = np.cov(mat1)
         cov_mat2 = np.cov(mat2)        
-    (sign0, logdet0) = np.linalg.slogdet(cov_mat0)
-    (sign1, logdet1) = np.linalg.slogdet(cov_mat1)
-    (sign2, logdet2) = np.linalg.slogdet(cov_mat2)
-    ln_det_mat0 = logdet0
-    ln_det_mat1 = logdet1
-    ln_det_mat2 = logdet2
-    H_mat1 = 0.5*np.log(np.power((2*np.exp(1)*np.pi), n)) + 0.5*ln_det_mat1
-    H_mat2 = 0.5*np.log(np.power((2*np.exp(1)*np.pi), n)) + 0.5*ln_det_mat2
-    MI = 0.5*(ln_det_mat1 + ln_det_mat2 - ln_det_mat0)
-    return 2*MI - H_mat1 - H_mat2;
+    (sign0, logdet0) = np.linalg.slogdet(cov_mat0*2*np.exp(1)*np.pi)
+    (sign1, logdet1) = np.linalg.slogdet(cov_mat1*2*np.exp(1)*np.pi)
+    (sign2, logdet2) = np.linalg.slogdet(cov_mat2*2*np.exp(1)*np.pi)
+    MI = (logdet1 + logdet2 - logdet0)
+    return logdet0 - MI;
 
 def bhdist (mu1, mu2, mat1, mat2,reg=0):
     #Bhattacharyya_distance assuming normal distros
